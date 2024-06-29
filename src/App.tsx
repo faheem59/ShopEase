6719d91d1a69a5221eb865411ce5810b07394cd7
faheem59 <faheem@binmile.com>
2024-06-29 17:52:27 +0530

    Task completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import Home from './components/Pages/Home'
-import Cart from './components/Pages/Cart'
-import Navbar from './components/commonNavbar/Navbar'
+import AllRoute from './Routes/AllRoute'
 
 const App = () => {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} ></Route>
-          <Route path='/cart' element={<Cart />} ></Route>
-        </Routes>
-      </Router>
+      <AllRoute/>
     </>
   )
 }
---
+++ src/Routes/AllRoute.tsx
@@ -0,0 +1,20 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Home from '../components/Pages/Home'
+import Cart from '../components/Pages/Cart'
+import Navbar from '../components/commonNavbar/Navbar'
+
+const AllRoute = () => {
+    return (
+        <>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Home />} ></Route>
+                    <Route path='/cart' element={<Cart />} ></Route>
+                </Routes>
+            </Router>
+        </>
+    )
+}
+
+export default AllRoute;
